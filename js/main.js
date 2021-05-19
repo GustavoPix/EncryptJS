c29360c012db9c221d2e3bd2bbb868bd22dbbe5e
Gustavo Carvalho <gustavo1net@hotmail.com>
2021-05-18 21:04:37 -0300

    Criptogradia, descriptografia e geracao de senha

--- js/main.js
+++ js/main.js
@@ -1,18 +1,37 @@
 const vm_main = new Vue({
     el: "#main",
     data:{
-        input:"",
+        hash:"",
+        pass:"",
         key:"",
-        result:""
+        resultPass:"",
+        resultHash:"",
+        newPass:"",
+        newHash:"",
+        aba:0
     },
     methods: {
         encrypt(){
-            let hash = CryptoJS.AES.encrypt(this.input,this.key);
-            this.result = hash.toString();
+            let hash = CryptoJS.AES.encrypt(this.pass,this.key);
+            this.resultPass = hash.toString();
         },
         decrypt(){
-            let dec = CryptoJS.AES.decrypt(this.input,this.key);
-            this.result = dec.toString(CryptoJS.enc.Utf8);
+            let dec = CryptoJS.AES.decrypt(this.hash,this.key);
+            this.resultHash = dec.toString(CryptoJS.enc.Utf8);
+        },
+        generatePass()
+        {
+            let charset="qwertyuiopasdfghjklçzxcvbnm1234567890!@#$%¨&*()-_=+;?";
+            let newPass = '';
+            for(let i = 0; i < 16; i++)
+            {
+                newPass+=this.getRandom(charset);
+            }
+            this.newPass = newPass;
+        },
+        getRandom(string)
+        {
+            return string[Math.floor(Math.random()*string.length)];
         }
     },
 });
